ba279dff1a2128eb43d8a424b663493e60ad985c
RodrigoEduardo347 <rodrigoeduardo347@gmail.com>
2022-04-12 07:13:33 -0300

    primeiro commit

--- alg2-ex-propostos-3/ex-proposto-2/exercicio-2.c
+++ alg2-ex-propostos-3/ex-proposto-2/exercicio-2.c
@@ -4,3 +4,49 @@ se a segunda é um anagrama da primeira. Uma palavra é um anagrama de outra
 se todas as letras de uma ocorrem na outra, em mesmo número, independente da
 posição. Exemplos: ROMA, MORA, ORAM, AMOR, RAMO, são anagramas entre si.
 */
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char palavra_um[40];
+    char palavra_dois[40];
+
+    printf("Digite a primeira palavra: ");
+    fgets(palavra_um, 40, stdin);
+    printf("\nDigite a segunda palavra: ");
+    fgets(palavra_dois, 40, stdin);
+
+    int quantidade_palavra_um = strlen(palavra_um);
+    int quantidade_palavra_dois = strlen(palavra_dois);
+    int quantidade_de_caracteres = quantidade_palavra_um == quantidade_palavra_dois;
+
+    if(quantidade_de_caracteres)
+    {
+        int cont = 0;
+        for(int i = 0; i < quantidade_palavra_um; i++)
+        {
+            for(int j = 0; j < quantidade_palavra_um; j++)
+            {
+                if(palavra_um[i] == palavra_dois[j])
+                {
+                    cont++;
+                    break;
+                }
+            }
+            if(quantidade_palavra_um == cont)
+            {
+                printf("\nAs palavras sao anagramas\n");
+            }
+            else{
+                printf("\nAs palavras não são anagramas!\n");
+            }
+        }
+    }
+    else
+    {
+        printf("\nquantidade diferente\n");
+    }
+
+}
