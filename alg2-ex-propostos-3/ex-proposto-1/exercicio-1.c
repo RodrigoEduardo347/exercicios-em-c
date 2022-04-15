82fd8709eba0341f42b0577573db0196d9404339
RodrigoEduardo347 <rodrigoeduardo347@gmail.com>
2022-04-15 10:02:35 -0300

    exercicio 1

--- alg2-ex-propostos-3/ex-proposto-1/exercicio-1.c
+++ alg2-ex-propostos-3/ex-proposto-1/exercicio-1.c
@@ -1,3 +1,4 @@
+   
 /*
 Exercício 1. Faça um programa que leia um valor n e depois leia uma sequência
 de n números inteiros e os armazene em um vetor. Posteriormente, seu programa
@@ -5,17 +6,28 @@ deve determinar o maior e o menor elemento do vetor.
 */
 
 #include <stdio.h>
+#include <locale.h>
 
-int main()
-{
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+    
     int n[100000];
     char continuar;
     int cont = 0;
+    int maior = 0;
+    int menor = 10000;
     
     do
     {
         printf("Digite um número: ");
         scanf(" %d", &n[cont]);
+        
+        if(maior < n[cont]){
+            maior = n[cont];
+        }
+        if(menor > n[cont]){
+            menor = n[cont];
+        }
 
         printf("Deseja adicionar mais números? (s/n)\n");
         scanf(" %c", &continuar);
@@ -24,23 +36,6 @@ int main()
         cont++;
 
     } while (continuar != 'n' && continuar != 'N');
-
-    int maior;
-    int menor;
-
-    maior = menor = n[0];
-
-    for(int i = 1; i < cont; i++)
-    {
-        if(n[i] > maior)
-        {
-            maior = n[i];
-        }
-        if(menor > n[i]) 
-        {
-            menor = n[i];
-        }
-    }
 
     printf("\nMaior valor: %d\nMenor valor: %d\n", maior, menor);
     
