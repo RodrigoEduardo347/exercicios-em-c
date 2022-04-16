168a4f5551c3339ee6ffd4e112dedacc45f46bae
RodrigoEduardo347 <rodrigoeduardo347@gmail.com>
2022-04-16 11:02:45 -0300

    último commit

--- alg2-ex-propostos-3/ex-proposto-6/exercicio-6.c
+++ alg2-ex-propostos-3/ex-proposto-6/exercicio-6.c
@@ -35,12 +35,13 @@ int main(){
     int cont_negativo = 0;
     for(int k = 0; k < i; k++){
         if(pesquisa == num[k]){
-            printf("\nO número está na posição %d\n", k+1);
+            printf("\nO número está na posição %d.", k+1);
         }else{
             cont_negativo++;
         }
     }
 
+    //Se o número não estiver em nenhuma posição do vetor
     if(cont_negativo == i){
         printf("\nNão conseguimos encontrar o seu número na lista :/\n");
     }
