ddfda7e34ebfd186731464c376027c49639e77d6
RodrigoEduardo347 <rodrigoeduardo347@gmail.com>
2022-04-16 10:03:44 -0300

    exercicio 6

--- alg2-ex-propostos-3/ex-proposto-6/exercicio-6.c
+++ alg2-ex-propostos-3/ex-proposto-6/exercicio-6.c
@@ -1,7 +1,40 @@
 /*
-Exercício 6. (Busca sequencial) Faça um programa que leia uma sequência de
-números positivos terminada com o número 0 e os armazene em um vetor.
-Depois disso, seu programa deve solicitar um número ao usuário e pesquisar se o
-número digitado ocorre no vetor. Seu programa deve implementar o algoritmo de
-busca sequencial.
+Exercício 6. (Busca sequencial) Faça um programa que leia uma sequência de números positivos terminada com o número 0 e os armazene em um vetor.
+Depois disso, seu programa deve solicitar um número ao usuário e pesquisar se o número digitado ocorre no vetor. Seu programa deve implementar o algoritmo de busca sequencial.
 */
+
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+
+    int i = 0;
+    int num[100000];
+    while(1){
+        printf("Digite um número: ");
+        scanf("%d", &num[i]);
+
+        if(num[i] == 0){
+            break;
+        }
+
+        if(num[i] < 0){
+            continue;
+        }
+        i++;
+    }
+
+    int pesquisa;
+
+    printf("\n\nDigite o número que deseja pesquisar: ");
+    scanf("%d", &pesquisa);
+
+    for(int k = 0; k < i; k++){
+        if(pesquisa == num[k]){
+            printf("\nO número está na posição %d\n", k+1);
+        }
+    }
+
+    return 0;
+}
