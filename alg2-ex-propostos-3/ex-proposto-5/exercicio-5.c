b21fcd5da132e40b42370ec31d3b3d7a723ab277
RodrigoEduardo347 <rodrigoeduardo347@gmail.com>
2022-04-20 07:27:22 -0300

    exercicio 5

--- alg2-ex-propostos-3/ex-proposto-5/exercicio-5.c
+++ alg2-ex-propostos-3/ex-proposto-5/exercicio-5.c
@@ -4,3 +4,52 @@ Exercício 5. Dadas duas sequências de n e m elementos binários (0 ou 1), onde
 na segunda. Ex: se a primeira for 101 e a segunda 1101010011010, então o
 resultado deve ser 3.
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//letura das sequências
+void leitura(char primeirasequencia[], char segundasequencia[]) {
+    printf("Digite a primera sequencia: ");
+    fflush(stdin);
+    fgets(primeirasequencia,100,stdin);
+    printf("Digite a segunda sequencia: ");
+    fflush(stdin);
+    fgets(segundasequencia,100,stdin);
+}
+
+int main(){
+
+    char n[100];
+    char m[100];
+    int vezes=0;
+
+    leitura(n, m);
+
+    //conta o tamanho de cada char tirando o espaço no buffer e o enter
+    int tamanhoN = strlen(n)-2;
+    int tamanhoM = strlen(m)-2;
+
+    if(tamanhoN > tamanhoM){
+        printf("a primeria sequencia deve ser menor que a segunda.");
+        return 0;
+    }
+
+    for(int i=0; i<=tamanhoM; i++){
+            int aparece=0;
+
+        for(int j=0; j<=tamanhoN; j++){
+            if(m[i+j] == n[j]){
+                aparece++;
+            }
+        }
+
+        if(aparece == tamanhoN+1){
+            vezes++;
+        }
+    }
+
+    printf("\nA primeira sequencia aparece %i vez(es) na segunda sequencia.",vezes);
+    return 0;
+}
